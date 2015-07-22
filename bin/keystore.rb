f4fcccb8eb8deed24ed266ad7959ac1ade1cd11c
jonny sywulak <jonny@stelligent.com>
2015-07-21 21:49:25 -0400

    adding script to do storing and retrieval from command line

---
+++ bin/keystore.rb
@@ -0,0 +1,53 @@
+require_relative '../lib/keystore'
+require 'aws-sdk-core'
+require 'trollop'
+
+SUB_COMMANDS = %w(store retrieve)
+global_opts = Trollop.options do
+  opt :region, 'The region to look for the dynamodb in', default: 'us-east-1'
+  banner 'utility for storing and retrieving encrypted values
+  available commands:
+
+    store -- store a value in keystore
+    retrieve -- retrieve a value from keystore
+
+  use --help with either command for more information.
+  '
+  stop_on SUB_COMMANDS
+end
+
+cmd = ARGV.shift
+cmd_opts =
+  case cmd
+  when 'store'
+    Trollop.options do
+      opt :value, 'the value to be inserted into the keystore (required for store)', required: true, type: String
+      opt :kmsid, 'the kms key id to use to encrypt the data (required for store)', required: true, type: String
+      opt :key, 'the name of the key associated with the value', required: true, type: String
+      opt :table, 'the name of the table to perform the lookup on', required: true, type: String
+    end
+  when 'retrieve'
+    Trollop.options do
+      opt :key, 'the name of the key associated with the value', required: true, type: String
+      opt :table, 'the name of the table to perform the lookup on', required: true, type: String
+    end
+  else
+    Trollop.die "unknown subcommand #{cmd.inspect}"
+  end
+
+dynamo = Aws::DynamoDB::Client.new region: global_opts[:region]
+kms = Aws::KMS::Client.new region: global_opts[:region]
+keystore = Keystore.new dynamo: dynamo, table_name: cmd_opts[:table], kms: kms, key_id: cmd_opts[:key]
+
+case cmd 
+when 'store'
+  puts "storing #{dynamo} #{cmd_opts[:table]}, #{kms}, #{cmd_opts[:key]}, #{cmd_opts[:value]}"
+  keystore = Keystore.new dynamo: @dynamo, table_name: @table_name, kms: @kms, key_id: @key_id
+  keystore.store key: @key, value: @value
+when 'retrieve'
+  puts "retrieving #{dynamo} #{cmd_opts[:table]}, #{cmd_opts[:key]}"
+  keystore = Keystore.new dynamo: @dynamo, table_name: @table_name, kms: @kms
+  @result = keystore.retrieve key: @key
+else
+  raise "unknown subcommand #{cmd}"
+end
