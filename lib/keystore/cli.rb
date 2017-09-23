cfa8628eb1266bee2cd19d01ffb5a9aabd94ab05
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2017-09-22 22:58:33 -0400

    refactor

--- lib/keystore/cli.rb
+++ lib/keystore/cli.rb
@@ -5,8 +5,11 @@ begin
 rescue LoadError
   nil
 end
+require 'english'
 require 'keystore'
 require 'optparse'
+
+# rubocop:disable ClassLength
 
 class Keystore
   # keystore CLI handler
@@ -19,11 +22,11 @@ class Keystore
     def init_clients
       @dynamo = Aws::DynamoDB::Client.new region: @opts[:region]
       @kms = Aws::KMS::Client.new region: @opts[:region]
-      @keystore = Keystore.new dynamo: @dynamo,
+      @keystore = Keystore.new dynamo: @dynamo, kms: @kms,
                                table_name: @opts[:table],
-                               kms: @kms,
                                key_id: @opts[:kmsid],
-                               key_alias: @opts[:kmsalias]
+                               key_alias: @opts[:kmsalias],
+                               keystore_format: @opts[:keystore_format]
     end
 
     def run
@@ -31,9 +34,9 @@ class Keystore
       init_clients
 
       case @opts[:verb]
-      when 'store'
+      when :store
         @keystore.store key: @opts[:keyname], value: @opts[:value]
-      when 'retrieve'
+      when :retrieve
         puts @keystore.retrieve(key: @opts[:keyname])
       else
         raise "unknown subcommand #{@opts[:verb]}"
@@ -72,49 +75,73 @@ class Keystore
 
     # rubocop:disable Metrics/MethodLength
 
-    def parse_arguments
-      subcommands = {
-        'store' => OptionParser.new do |opts|
-          opts.banner = "Usage: #{$PROGRAM_NAME} store [options]"
-          opts.on('-v', '--value=VALUE',
-                  'Value to store in keystore') do |v|
-            @opts[:value] = v
-          end
-          opts.on('-k', '--kmsid=KMSID',
-                  'ID of KMS CMK to use') do |k|
-            @opts[:kmsid] = k
-          end
-          opts.on('--kmsalias=KMSALIAS',
-                  'Alias of KMS CMK to use') do |k|
-            @opts[:kmsalias] = k
-          end
-          opts.on('-e', '--keyname=KEY',
-                  'keystore key to retrieve/store') do |e|
-            @opts[:keyname] = e
-          end
-          opts.on('-t', '--table=TABLE',
-                  'DynamoDB table to retrieve/store from') do |t|
-            @opts[:table] = t
-          end
-        end,
-        'retrieve' => OptionParser.new do |opts|
-          opts.banner = "Usage: #{$PROGRAM_NAME} retrieve [options]"
-          opts.on('-k', '--keyname=KEY',
-                  'keystore key to retrieve/store') do |k|
-            @opts[:keyname] = k
-          end
-          opts.on('-t', '--table=TABLE',
-                  'DynamoDB table to retrieve/store from') do |t|
-            @opts[:table] = t
-          end
-        end
+    def subcommands_args
+      {
+        store: {
+          op: OptionParser.new do |opts|
+            opts.banner = "Usage: #{$PROGRAM_NAME} store [options]"
+            opts.on('-v', '--value=VALUE',
+                    'Value to store in keystore') do |v|
+              @opts[:value] = v
+            end
+            opts.on('-k', '--kmsid=KMSID',
+                    'ID of KMS CMK to use') do |k|
+              @opts[:kmsid] = k
+            end
+            opts.on('--kmsalias=KMSALIAS',
+                    'Alias of KMS CMK to use') do |k|
+              @opts[:kmsalias] = k
+            end
+            opts.on('-e', '--keyname=KEY',
+                    'keystore key to retrieve/store') do |e|
+              @opts[:keyname] = e
+            end
+            opts.on('-t', '--table=TABLE',
+                    'DynamoDB table to retrieve/store from') do |t|
+              @opts[:table] = t
+            end
+          end,
+          mandatory: %i[value keyname table]
+        },
+        retrieve: {
+          op: OptionParser.new do |opts|
+            opts.banner = "Usage: #{$PROGRAM_NAME} retrieve [options]"
+            opts.on('-k', '--keyname=KEY',
+                    'keystore key to retrieve/store') do |k|
+              @opts[:keyname] = k
+            end
+            opts.on('-t', '--table=TABLE',
+                    'DynamoDB table to retrieve/store from') do |t|
+              @opts[:table] = t
+            end
+          end,
+          mandatory: %i[keyname table]
+        }
       }
-
-      global_args.order! do |command|
-        @opts[:verb] = command
-        subcommands[command].order!
-      end
     end
     # rubocop:enable Metrics/MethodLength
+
+    # rubocop:disable Metrics/MethodLength
+
+    def parse_arguments
+      argv_subcommand = (optparse = global_args).order(argv)
+      command = @opts[:verb] = argv_subcommand.shift.to_sym
+      subcommand = subcommands_args[command]
+      raise OptionParser::MissingArgument unless subcommand
+      optparse = subcommand[:op]
+      optparse.order!(argv_subcommand)
+      missing = subcommand[:mandatory].select { |param| @opts[param].nil? }
+      unless missing.empty?
+        raise OptionParser::MissingArgument, missing.join(', ')
+      end
+    rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+      puts $ERROR_INFO.to_s + "\n" + optparse
+      exit
+    end
+    # rubocop:enable Metrics/MethodLength
+
+    private
+
+    def argv; ARGV; end # rubocop:disable SingleLineMethods
   end
 end
--- spec/keystore_cli_spec.rb
+++ spec/keystore_cli_spec.rb
@@ -2,7 +2,34 @@ require 'keystore/cli'
 
 describe Keystore::Cli do
   it 'initializes correctly' do
+    cli_startopts = { region: 'us-east-1' }
     s = Keystore::Cli.new
     expect(s).to be_kind_of(Keystore::Cli)
+    expect(s.instance_variable_get(:@opts)).to eq cli_startopts
+  end
+  describe 'Keystore::Cli.run' do
+    it 'invokes v1 store correctly' do
+      cli = Keystore::Cli.new
+      dynamo = Aws::DynamoDB::Client.new region: 'us-east-1'
+      kms = Aws::KMS::Client.new region: 'us-east-1'
+      keystore = Keystore.new dynamo: dynamo,
+                              table_name: 'notable',
+                              kms: kms,
+                              key_id: 'nokeyid',
+                              key_alias: nil
+      cli.instance_variable_set(:@keystore, keystore)
+      def cli.init_clients; end
+      expect(keystore).to(
+        receive(:store).with(key: 'dummykey', value: 'dummyvalue')
+      )
+      def cli.argv
+        %w[store -t notable -k nokeyid -e dummykey -v dummyvalue]
+      end
+      cli.run
+    end
+    it 'invokes v2 correctly' do
+    end
+    it 'defaults to us-east-1' do
+    end
   end
 end
